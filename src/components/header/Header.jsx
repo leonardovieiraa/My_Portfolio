cc478e38c293ed7ea53cc39d772ec5e08b59e8a5
Leonardo Vieira <102541809+leonardovieiraa@users.noreply.github.com>
2024-10-15 19:33:56 -0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
     <header id="home">
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Christian Montero</h1>
-        <h5 className="text-light">Full-Stack Developer</h5>
+        <h1>Leonardo</h1>
+        <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
